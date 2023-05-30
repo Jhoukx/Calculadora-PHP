9d88e514fe2cdd0f9c80a3c48492e5cc1d2e7135
John <jegonzalezesparragoza@gmail.com>
2023-05-30 17:40:57 -0500

    feat: :sparkles: Primera clase
    El proceso lo hace en back pero falta por mostrar en el front

---
+++ api.php
@@ -0,0 +1,28 @@
+<?php
+    echo <<<HTML
+        Hola Pana <br><a href="index.html" target="_self">Volver</a><br>
+    HTML;
+    echo "<pre>";
+    var_dump($_POST);
+    echo "<hr>";
+    $numero1 =(int)($_POST["numero1"]);
+    $numero2 =(int)($_POST["numero2"]);
+    $operacion = ($_POST["operacion"]);
+    $resultado = 0;
+    switch($operacion){
+        case "suma":
+            $resultado=$numero1 + $numero2; 
+            break;      
+        case "resta":
+            echo "Se hace una resta";
+            $resultado = $numero1-$numero2;
+            break;
+        case "multiplicacion":
+            $resultado = $numero1 * $numero2;
+            break;
+        case "division":
+            $resultado = $numero1/$numero2;
+            break;
+    }
+    echo $resultado;
+?>
