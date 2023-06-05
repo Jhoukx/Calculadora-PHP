70c405ff23dad4645ea83340ba823e6ec7bda9bd
John González <jegonzalezesparragoza@gmail.com>
2023-06-05 09:18:48 -0500

    feat: :sparkles: Calculadora funcional pero necesita formatear numeros enteros
    La calculadora funciona operacion por operacion

--- index.php
+++ index.php
@@ -2,12 +2,54 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-var_dump($_POST);
-$texto = null;
+
+
+$texto = 0;
+session_start();
+//Unir caracteres
 if (isset($_POST["botones"])){
-    $texto =$_POST["resultado"]. $_POST["botones"];
+    $texto =intval($_POST["resultado"]. $_POST["botones"]);
+}
+//Almacenar operacion
+if (isset($_POST["operacion"])){
+    $num1 =  floatval($_POST["resultado"]);
+    $_SESSION["num1"]=$num1;
+    $_SESSION["signo"]= $_POST["operacion"];
+    $texto = "";
+}
+//Resultados
+if (isset($_POST["igual"])){
+    $num2 = floatval($_POST["resultado"]);
+
+    if ($_SESSION["signo"]==="*"){
+        $texto = number_format(($_SESSION["num1"] * $num2),2);
+    }else if ($_SESSION["signo"]=="-"){
+        $texto = number_format(($_SESSION["num1"] - $num2),2);
+    }else if ($_SESSION["signo"]=="+"){
+        $texto = number_format(($_SESSION["num1"] + $num2),2);
+    }else if ($_SESSION["signo"]==="/"){
+        $texto = number_format(($_SESSION["num1"]/$num2),2);
+    }
+    
     
 }
+// Usar boton flecha
+if (isset($_POST["funciones"])){
+    if($_POST["funciones"]==="←"){
+        $numeros = $_POST["resultado"];
+        $texto = substr($numeros, 0, -1);
+        //Usar boton C
+    }else if ($_POST["funciones"]==="c"){
+        $texto = "";
+    }else if ($_POST["funciones"]==="π"){
+        $texto = M_PI;
+    }else if ($_POST["funciones"]==="²"){
+        $numElevado = floatval($_POST["resultado"]);
+        $texto = $numElevado**2;
+
+    }
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,7 +59,7 @@ if (isset($_POST["botones"])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <link rel="stylesheet" href="./index.css">
+    <link rel="stylesheet" href="./file.css">
 </head>
 <body>
     <div class="calculadora">
@@ -30,33 +72,33 @@ if (isset($_POST["botones"])){
             <div class="row">
                 <div class="col">
                         <div class="row">
-                            <button type="submit" class="borrador" name="botones"  value="←">←</button>
-                            <button type="submit" class="botones" name="botones"  value="π">π</button>
-                            <button type="submit" class="botones" name="botones"  value="*">*</button>
-                            <button type="submit" class="botones" name="botones"  value="-">-</button>
+                            <button type="submit" class="borrador" name="funciones"  value="←">←</button>
+                            <button type="submit" class="botones" name="funciones"  value="π">π</button>
+                            <button type="submit" class="botones" name="operacion"  value="*">*</button>
+                            <button type="submit" class="botones" name="operacion"  value="-">-</button>
                         </div>
                         <div class="row">
                             <button type="submit" class="botones" name="botones"  value="7">7</button>
                             <button type="submit" class="botones" name="botones"  value="8">8</button>
                             <button type="submit" class="botones" name="botones"  value="9">9</button>
-                            <button type="submit" class="botones" name="botones"  value="+">+</button>
+                            <button type="submit" class="botones" name="operacion"  value="+">+</button>
                         </div>
                         <div class="row">
                             <button type="submit" class="botones" name="botones"  value="4">4</button>
                             <button type="submit" class="botones" name="botones"  value="5">5</button>
                             <button type="submit" class="botones" name="botones"  value="6">6</button>
-                            <button type="submit" class="botones" name="botones"  value=" /">/</button>
+                            <button type="submit" class="botones" name="operacion"  value="/">/</button>
                         </div>
                         <div class="row" style="margin-bottom: -66px;">
                             <button type="submit" class="botones" name="botones"  value="1">1</button>
                             <button type="submit" class="botones" name="botones"  value="2">2</button>
                             <button type="submit" class="botones" name="botones"  value="3">3</button>
-                            <button type="submit" name="botones"  id="igual" value="=">=</button>
+                            <button type="submit" name="igual"  id="igual" value="=">=</button>
                         </div>
                         <div class="row ultimos">
-                            <button type="submit" class="botones" name="botones"  value="4">0</button>
-                            <button type="submit" class="botones" name="botones"  value="²">X²</button>
-                            <button type="submit" class="botones" name="botones"  value="5">c</button>
+                            <button type="submit" class="botones" name="botones"  value="0">0</button>
+                            <button type="submit" class="botones" name="funciones"  value="²">X²</button>
+                            <button type="submit" class="botones" name="funciones"  value="c">CE</button>
                         </div>
                     </form>
                 </div>
